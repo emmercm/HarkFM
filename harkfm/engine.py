emmercm/HarkFM

commit 9c5e5b871b5b3fbad8a8673a3775f96fa4893023

Implemented a basic modal to edit existing settings.

harkfm/engine.py
@@ -17,8 +17,10 @@ class Engine(object):
     _thread_scrobbler = None
 
     config = None
-    logger = None
     correct_thread = None
+
+    storage = None
+    logger = None
 
     def __init__(self):
         if self.__class__.config is None:
@@ -26,6 +28,8 @@ class Engine(object):
             json_file = os.path.splitext(__file__)[0] + '.json'
             self.__class__.config = harkfm.Util.json_load(json_file)
 
+        if self.__class__.storage is None:
+            self.__class__.storage = harkfm.Storage()
         if self.__class__.logger is None:
             self.__class__.logger = logging.getLogger('root')
 
@@ -62,28 +66,36 @@ class Engine(object):
         return False
 
     def scrobbler_log(self):
-        storage = harkfm.Storage()
-
         def scrobbler_log_do(upd):
             while True:
-                if self.current is not None and self.__class__._lfm_network is not None:
+                if (
+                    self.current is not None
+                    and self.__class__._lfm_network is not None
+                    and self.__class__.storage.config_get('settings/scrobble/enabled')
+                ):
                     elapsed_percent = (time.time() - self.current.start) / self.current.track_duration * 100
                     # Listen
-                    if self.current and elapsed_percent >= storage.config_get('TODO/SOMETHING/HERE', 2):
+                    if (
+                        self.current
+                        and not self.current.listened
+                        and elapsed_percent >= self.__class__.storage.config_get('settings/scrobble/listen_percent')
+                    ):
                         self.current.listen()
                     # Queue scrobble
-                    if self.current and not self.current.queued and elapsed_percent >= storage.config_get('TODO/SOMETHING/HERE', 33):
-                        storage.config_append('queue', jsonpickle.encode(self.current))
+                    if (
+                        self.current
+                        and not self.current.queued
+                        and elapsed_percent >= self.__class__.storage.config_get('settings/scrobble/scrobble_percent')
+                    ):
+                        self.__class__.storage.config_append('queue', jsonpickle.encode(self.current))
                         self.current.queued = int(time.time())
                 time.sleep(0.5)
         if self.__class__._thread_scrobbler_log is None:
             self.__class__._thread_scrobbler_log = harkfm.Util.thread(scrobbler_log_do, None, None)
 
     def scrobble(self):
-        storage = harkfm.Storage()
-
         def scrobble_do(upd):
-            queue = storage.config_get('queue')
+            queue = self.__class__.storage.config_get('queue')
             if type(queue) is list and len(queue) > 0:
                 while queue:
                     # Consume and scrobble
@@ -92,16 +104,17 @@ class Engine(object):
                     if not track.scrobble():
                         queue.insert(0, item)
                         break
-                storage.config_set('queue', queue)
-        if self.__class__._lfm_network is not None and \
-                (self.__class__._thread_scrobbler is None or not self.__class__._thread_scrobbler.isRunning()):
+                        self.__class__.storage.config_set('queue', queue)
+        if (
+            self.__class__._lfm_network is not None
+            and self.__class__._thread_scrobbler is None or not self.__class__._thread_scrobbler.isRunning()
+        ):
             self.__class__._thread_scrobbler = harkfm.Util.thread(scrobble_do, None, None)
 
     def lfm_login(self):
-        storage = harkfm.Storage()
         if self.__class__._lfm_network is None:
-            username = storage.config_get('apis/last.fm/username')
-            session_key = storage.config_get('apis/last.fm/session_key')
+            username = self.__class__.storage.config_get('apis/last.fm/username')
+            session_key = self.__class__.storage.config_get('apis/last.fm/session_key')
             if session_key is not None:
                 api_key = harkfm.Engine.config['apis']['last.fm']['key']
                 api_secret = harkfm.Engine.config['apis']['last.fm']['secret']
@@ -119,16 +132,15 @@ class Engine(object):
                 user = self.__class__._lfm_network.get_authenticated_user()
                 if user is not None:
                     self.__class__._lfm_network.username = user.get_name()
-                    storage.config_set('apis/last.fm/username', self.__class__._lfm_network.username)
+                    self.__class__.storage.config_set('apis/last.fm/username', self.__class__._lfm_network.username)
             except Exception as e:
                 self.__class__.logger.warn('%s  %s', type(e), e)
 
         return self.__class__._lfm_network
 
     def lfm_logout(self):
-        storage = harkfm.Storage()
-        storage.config_set('apis/last.fm/username', None)
-        storage.config_set('apis/last.fm/session_key', None)
+        self.__class__.storage.config_set('apis/last.fm/username', None)
+        self.__class__.storage.config_set('apis/last.fm/session_key', None)
         self.__class__._lfm_network = None
         self.current = None
         interface = harkfm.Interface()
@@ -141,7 +153,12 @@ class Engine(object):
             return self.__class__._lfm_network.__dict__
 
     def tts(self):
-        if self.current and self.current.track and self.current.artist:
+        if (
+            self.current
+            and self.current.track
+            and self.current.artist
+            and self.__class__.storage.config_get('settings/tts/enabled')
+        ):
             speech = self.current.track + ' by ' + self.current.artist
             if os.name == 'nt':
                 if self.__class__._tts is None:

harkfm/interface.py
@@ -1,4 +1,6 @@
 import copy
+import json
+import logging
 import math
 import os
 import sys
@@ -24,7 +26,12 @@ class Interface(object):
     qMainWindow = None
     qtDesigner = None
 
+    logger = None
+
     def __init__(self, qMainWindow=None, qtDesigner=None):
+        if self.__class__.logger is None:
+            self.__class__.logger = logging.getLogger('root')
+
         if self.__class__.loader is None:
             self.__class__.loader = genshi.template.TemplateLoader(
                 os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), 'www')),
@@ -124,12 +131,21 @@ class Interface(object):
                     interface = harkfm.Interface()
                     interface.index()
                 except Exception as e:
-                    print(e)
+                    self.__class__.logger.error('%s  %s', type(e), e)
 
-            @QtCore.pyqtSlot(QWebElement)
+            @QtCore.pyqtSlot()
             def logout(self):
                 engine = harkfm.Engine()
                 engine.lfm_logout()
+
+            @QtCore.pyqtSlot(str)
+            def save_settings(self, form):
+                form = json.loads(form)
+                storage = harkfm.Storage()
+                for key in form:
+                    storage.config_set('settings/'+key, form[key])
+                interface = harkfm.Interface()
+                interface.index()
 
         ext = Extensions(self.__class__.qMainWindow)
 
@@ -140,11 +156,15 @@ class Interface(object):
             current._elapsed = ext.elapsed()
             current._percent = ext.percent()
             current._remaining = ext.remaining()
-        lastfm = engine.lfm_props()
 
+        # Render page
         template = self.__class__.loader.load(file)
         search_path = 'file:///' + self.__class__.loader.search_path[0].replace('\\', '/') + '/'
-        html = template.generate(current=current, lastfm=lastfm).render('html', doctype='html')
+        html = template.generate(
+            current=current,
+            lastfm=engine.lfm_props(),
+            config=harkfm.Storage.config
+        ).render('html', doctype='html')
         self.__class__.qtDesigner.webView.setHtml(html, QUrl(search_path))
         self.__class__.qtDesigner.webView.page().mainFrame().addToJavaScriptWindowObject('py', ext)
         self.__class__._last_page = file

harkfm/scanner.py
@@ -12,23 +12,28 @@ import harkfm
 class Scanner(object):
     _config = None
 
+    storage = None
+
     def __init__(self):
-        if self._config is None:
+        if self.__class__._config is None:
             # Parse JSON config
             json_file = os.path.splitext(__file__)[0] + '.json'
-            self._config = harkfm.Util.json_load(json_file)
+            self.__class__._config = harkfm.Util.json_load(json_file)
             # Compile JSON config
-            if 'windows' in self._config:
-                for window_id, window in enumerate(self._config['windows']):
+            if 'windows' in self.__class__._config:
+                for window_id, window in enumerate(self.__class__._config['windows']):
                     for prop in window['window']:
-                        self._config['windows'][window_id]['window'][prop] = re.compile(window['window'][prop])
+                        self.__class__._config['windows'][window_id]['window'][prop] = re.compile(window['window'][prop])
                     for prop in window['regex']:
-                        self._config['windows'][window_id]['regex'][prop] = re.compile(window['regex'][prop])
-            if 'replace' in self._config:
-                for category in self._config['replace']:
-                    for replace_id, replace in enumerate(self._config['replace'][category]):
-                        self._config['replace'][category][replace_id][0] = re.compile(
-                            self._config['replace'][category][replace_id][0])
+                        self.__class__._config['windows'][window_id]['regex'][prop] = re.compile(window['regex'][prop])
+            if 'replace' in self.__class__._config:
+                for category in self.__class__._config['replace']:
+                    for replace_id, replace in enumerate(self.__class__._config['replace'][category]):
+                        self.__class__._config['replace'][category][replace_id][0] = re.compile(
+                            self.__class__._config['replace'][category][replace_id][0])
+
+        if self.__class__.storage is None:
+            self.__class__.storage = harkfm.Storage()
 
         class ScannerThread(QThread):
             updated = QtCore.pyqtSignal(dict)
@@ -49,6 +54,9 @@ class Scanner(object):
                                 import win32gui
 
                                 def win_search(hwnd, win):
+                                    # Possibility of window closing in the middle of this enum
+                                    if not win32gui.IsWindow(hwnd):
+                                        return
                                     # Get window properties
                                     w_class = win32gui.GetClassName(hwnd)
                                     w_text = win32gui.GetWindowText(hwnd)
@@ -112,10 +120,11 @@ class Scanner(object):
                                 for prop in window['regex']:
                                     props[prop] = harkfm.Util.regex(window['regex'][prop], w_text)
                                     # Do replacements
-                                    for replace in self.scanner._config['replace']['all']:
-                                        props[prop] = re.sub(replace[0], replace[1], props[prop])
-                                    for replace in self.scanner._config['replace'][prop]:
-                                        props[prop] = re.sub(replace[0], replace[1], props[prop])
+                                    if self.scanner.storage.config_get('settings/correct/scanner'):
+                                        for replace in self.scanner._config['replace']['all']:
+                                            props[prop] = re.sub(replace[0], replace[1], props[prop])
+                                        for replace in self.scanner._config['replace'][prop]:
+                                            props[prop] = re.sub(replace[0], replace[1], props[prop])
 
                                 self.updated.emit(props)
 

harkfm/storage.py
@@ -15,12 +15,28 @@ class Storage(object):
             self.__class__.config_file = os.path.dirname(sys.argv[0]) + 'storage.json'
             self.__class__.config = harkfm.Util.json_load(self.__class__.config_file)
 
+            # Default setting values
+            self.config_init('settings/scrobble/enabled', True)
+            self.config_init('settings/scrobble/listen_percent', 2)
+            self.config_init('settings/scrobble/scrobble_percent', 33)
+            self.config_init('settings/correct/scanner', True)
+            self.config_init('settings/correct/gracenote', True)
+            self.config_init('settings/correct/last.fm', True)
+            self.config_init('settings/tts/enabled', True)
+
+    # Initialize a config value if it isn't already set
+    def config_init(self, path, value):
+        if self.config_get(path) is None:
+            self.config_set(path, value)
+            return True
+        return False
+
     # Get a value out of the config
     def config_get(self, path, default=None):
         path = path.split('/')
         item = self.__class__.config
         for key in path:
-            if key in item:
+            if hasattr(item, '__iter__') and key in item:
                 item = item[key]
             else:
                 return default
@@ -31,7 +47,7 @@ class Storage(object):
         path = path.split('/')
         item = self.__class__.config
         for key in path[:-1]:
-            if key not in item:
+            if key not in item or not hasattr(item[key], '__iter__'):
                 item[key] = {}
             item = item[key]
         if path[-1] not in item or item[path[-1]] != value:

harkfm/track.py
@@ -16,6 +16,7 @@ class Track(object):
     def __init__(self, props=None):
         if props is None:
             props = {}
+
         if self.__class__.storage is None:
             self.__class__.storage = harkfm.Storage()
         if self.__class__.engine is None:
@@ -129,20 +130,25 @@ class Track(object):
         def gn_end():
             self.__correct_lfm()
 
-        if not self._corrected_gn:
-            harkfm.Util.thread(gn_do, gn_upd, gn_end)
-            self._corrected_gn = True
-        else:
-            self.__correct_lfm()
+        if (
+            self._corrected_gn
+            or not self.__class__.storage.config_get('settings/correct/gracenote')
+        ):
+            return self.__correct_lfm()
+
+        harkfm.Util.thread(gn_do, gn_upd, gn_end)
+        self._corrected_gn = True
 
     def __correct_lfm(self):
         lfm_network = self.__class__.engine.lfm_login()
-        if lfm_network is None:
+        if (
+            lfm_network is None
+            or self._corrected_lfm
+            or not self.__class__.storage.config_get('settings/correct/last.fm')
+        ):
             return
-        if self._corrected_lfm:
-            return
-        self._corrected_lfm = True
 
+        self._corrected_lfm = True
         lfm_count = 0
 
         def lfm_end():

www/header.html
@@ -7,7 +7,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">About HarkFM</h4>
+                    <h4 class="modal-title">About</h4>
                 </div>
                 <div class="modal-body">
                     <div class="row">
@@ -31,6 +31,61 @@
         </div>
     </div>
 
+    <div class="modal fade" id="settings" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Settings</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="scrobble/enabled" py:attrs="{'checked':'checked' if config['settings']['scrobble']['enabled'] else ''}" /> Enable scrobbling
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="correct/scanner" py:attrs="{'checked':'checked' if config['settings']['correct']['scanner'] else ''}" /> Enable window scanner track correction
+                                </label>
+                            </div>
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="correct/gracenote" py:attrs="{'checked':'checked' if config['settings']['correct']['gracenote'] else ''}" /> Enable <a href="http://www.gracenote.com">Gracenote</a> track correction
+                                </label>
+                            </div>
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="correct/last.fm" py:attrs="{'checked':'checked' if config['settings']['correct']['last.fm'] else ''}" /> Enable <a href="http://www.last.fm/home">Last.fm</a> track correction
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="tts/enabled" py:attrs="{'checked':'checked' if config['settings']['tts']['enabled'] else ''}" /> Enable verbal track announcements
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-primary" onclick="save_settings(this);">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     <div class="navbar navbar-fixed-top bar-app">
         <div class="row">
             <div class="app-icon">
@@ -43,16 +98,21 @@
                 </py:if>
                 <py:if test="lastfm">
                     <py:if test="not current">
-                        Hello, <strong>${lastfm['username']}!</strong>
+                        Hello,
+                        <br />
+                        <strong>${lastfm['username']}!</strong>
                     </py:if>
                     <py:if test="current">
-                        Scrobbling from <strong>${current.app}</strong>
+                        Scrobbling from
+                        <br />
+                        <strong>${current.app}</strong>
                     </py:if>
                 </py:if>
             </div>
             <div class="quick-icons">
-                <span id="button-about" class="fa fa-info-circle" title="About HarkFM" onclick="$('#about').modal();" keyup="112"></span>
-                <span id="button-logout" class="fa fa-sign-out" title="Log Out" onclick="py.logout(this);" py:if="lastfm"></span>
+                <span class="fa fa-info-circle" title="About (F1)" onclick="$('#about').modal();" keyup="112"></span>
+                <span class="fa fa-cog" title="Settings" onclick="$('#settings').modal();"></span>
+                <span class="fa fa-sign-out" title="Log Out" onclick="py.logout();" py:if="lastfm"></span>
             </div>
         </div>
     </div>

www/static/css/harkfm.css
@@ -42,6 +42,68 @@ h1, h2, h3, h4, h5, h6,
     margin: 15px 0;
 }
 
+.panel:last-child {
+    margin-bottom: 0;
+}
+
+.btn-primary {
+   color: #ffffff;
+   background-color: #d51007;
+   border-color: #bc0e06;
+}
+.btn-primary:focus,
+.btn-primary.focus {
+   color: #ffffff;
+   background-color: #a40c05;
+   border-color: #410502;
+}
+.btn-primary:hover {
+   color: #ffffff;
+   background-color: #a40c05;
+   border-color: #810a04;
+}
+.btn-primary:active,
+.btn-primary.active,
+.open > .dropdown-toggle.btn-primary {
+   color: #ffffff;
+   background-color: #a40c05;
+   border-color: #810a04;
+}
+.btn-primary:active:hover,
+.btn-primary.active:hover,
+.open > .dropdown-toggle.btn-primary:hover,
+.btn-primary:active:focus,
+.btn-primary.active:focus,
+.open > .dropdown-toggle.btn-primary:focus,
+.btn-primary:active.focus,
+.btn-primary.active.focus,
+.open > .dropdown-toggle.btn-primary.focus {
+   color: #ffffff;
+   background-color: #810a04;
+   border-color: #410502;
+}
+.btn-primary:active,
+.btn-primary.active,
+.open > .dropdown-toggle.btn-primary {
+   background-image: none;
+}
+.btn-primary.disabled:hover,
+.btn-primary[disabled]:hover,
+fieldset[disabled] .btn-primary:hover,
+.btn-primary.disabled:focus,
+.btn-primary[disabled]:focus,
+fieldset[disabled] .btn-primary:focus,
+.btn-primary.disabled.focus,
+.btn-primary[disabled].focus,
+fieldset[disabled] .btn-primary.focus {
+   background-color: #d51007;
+   border-color: #bc0e06;
+}
+.btn-primary .badge {
+   color: #d51007;
+   background-color: #ffffff;
+}
+
 /* Bootstrap rule ordering fix */
 .progress-bar.text-right {
     text-align: right;
@@ -114,7 +176,7 @@ h1, h2, h3, h4, h5, h6,
 .bar-app .row .quick-icons .glyphicon,
 .bar-app .row .quick-icons .fa {
     margin-top: 8px;
-    margin-left: 15px;
+    margin-left: 20px;
     font-size: 22px;
 }
 .bar-app + * {

www/static/js/harkfm.js
@@ -23,3 +23,19 @@ function unlove(reference) {
     $(reference).attr({'onclick':'', 'class':'fa fa-spinner fa-pulse'});
     py.unlove();
 }
+
+function save_settings(reference) {
+    var values = new Object();
+    $(reference).closest('.modal-dialog').find('input').each(function() {
+        var $input = $(this);
+        var value = $input.val();
+        switch($input.attr('type')) {
+            case 'checkbox':
+                value = $input.is(':checked');
+                break;
+        }
+        values[$input.attr('name')] = value;
+    });
+    py.save_settings(JSON.stringify(values));
+    $(reference).closest('.modal').modal('hide');
+}

commit d912d8035041479fb8d8a84f1b179b2ef555ff93

Fix scrobbling issues introduced in 9c5e5b8 and 336301f.

harkfm/engine.py
@@ -95,6 +95,7 @@ class Engine(object):
 
     def scrobble(self):
         def scrobble_do(upd):
+            harkfm.Track()  # init static classes
             queue = self.__class__.storage.config_get('queue')
             if type(queue) is list and len(queue) > 0:
                 while queue:
@@ -104,7 +105,7 @@ class Engine(object):
                     if not track.scrobble():
                         queue.insert(0, item)
                         break
-                        self.__class__.storage.config_set('queue', queue)
+                self.__class__.storage.config_set('queue', queue)
         if (
             self.__class__._lfm_network is not None
             and self.__class__._thread_scrobbler is None or not self.__class__._thread_scrobbler.isRunning()